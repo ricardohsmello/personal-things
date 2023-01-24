ffdc5e2707dada43b2f5c3743ffd3662cb556627
ricardohsmello <ricardohsmello@gmail.com>
2023-01-24 00:37:58 -0300

    first commit

---
+++ src/test/kotlin/br/com/ricas/personalthings/PersonalStuffsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ricas.personalthings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalthingsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
